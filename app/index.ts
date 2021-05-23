79b24880fae1ad6aa0537519b23c6ca2f7ca72e0
MJ Macheli <machelimail@gmail.com>
2021-05-23 22:09:50 +0200

    more setup

---
+++ app/index.ts
@@ -0,0 +1,32 @@
+import express, { Application, Request, Response } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get(
+    "/",
+    async (_ , res: Response): Promise<Response> => {
+        return res.status(200).send({
+            message: "Hello World!",
+        });
+    }
+);
+
+try {
+    app.listen(PORT, (): void => {
+        console.log(`Connected successfully on port ${PORT}`);
+    });
+} catch (error) {
+    console.error(`Error occured: ${error.message}`);
+}
